972a293718792f4901bd051d0ab21a935454dc6f
osoccol <51447258+osoccol@users.noreply.github.com>
2023-11-06 10:06:49 -0600

    CORS Error

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ const ENV = require('./environment/environment');
 const app = express();
 app.use(compression());
 
-
 const DB_id = ENV.DB_ID;
 const DB_pw = ENV.DB_PW;
 const DB = 'mongodb+srv://' + DB_id + ':' + DB_pw + '@clustertest.lw5japt.mongodb.net/';
+
+
+// Passby CORS errors
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 mongoose.connect(DB).then(() => {
     console.log('Connected to MongoDB with Success !');
